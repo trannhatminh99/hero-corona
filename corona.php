4a5e22c346577f8855560854ddecd06add9b6929
tnminhit99 <tnminh.it99@gmail.com>
2020-03-27 00:02:36 +0700

    update: Raw data

--- corona.php
+++ corona.php
@@ -1,47 +1,58 @@
 <?php
- require_once('./WebClient.php');
+require_once('./WebClient.php');
 $targetUrl = 'https://www.worldometers.info/coronavirus/';
 $webClient = new WebClient();
 
 $filenameBk = "get-data.txt";
-if ( file_exists($filenameBk) && ( filemtime($filenameBk) + (60 * 60)) < time() ) {
+if (file_exists($filenameBk) && (filemtime($filenameBk) + (60 * 60)) < time()) {
     $getData = file_get_contents($filenameBk);
 } else {
-    if( !($getData = $webClient->Navigate( $targetUrl )) ) {
-        trigger_error( "Could not load the index page.", E_USER_ERROR );  
+    if (!($getData = $webClient->Navigate($targetUrl))) {
+        trigger_error("Could not load the index page.", E_USER_ERROR);
     }
-  
-    
-    $myfile  = fopen($filenameBk, "w+") or die("Unable to open file!");
+
+
+    $myfile = fopen($filenameBk, "w+") or die("Unable to open file!");
     fwrite($myfile, $getData);
     fclose($myfile);
 }
-  $output = array();
-  preg_match("'<div class=\"container\"> (.*?)</div>'si", $getData, $getDataOutput);
-  
-  $dom = new DOMDocument();
+$output = array();
+preg_match_all("'<div class=\"tab-content\" [^>]+>(.*?)</div>'si", $getData, $getDataOutputTable);
 
-    $dom->loadHTML($getDataOutput[1]);
+$filenameTable = "data-table.txt";
 
-    $xpath = new DOMXPath($dom);
+if(file_exists($filenameTable) && (filemtime($filenameBk) + (60 * 60)) < time())
+{
+    
+} else {
+    $myfileTable = fopen($filenameTable, "w+") or die("Unable to open file!");
+    fwrite($myfileTable, $getDataOutputTable[0][0]);
+    fclose($myfileTable);
+}
 
-    $matches = $xpath->query('//div[@class="maincounter-number"]');
-   
-      $div = $div->item(0);
 
-echo $dom->saveXML($div);
+$dom = new DOMDocument();
 
- print_r($matches);exit;
- 
-  $getDataOutput = array();
-  preg_match_all('@<(div|em) class=\"maincounter-number\" (style=\"(.+?)\")>(.+?)</\1>@is', $getData, $matches);
- 
-  print_r($matches);exit;
-  $output['total']  = $getDataTotal[1];
-  
-  
-  $output['table']  = $getDataOutput[1];
-  
-  print_r($getDataOutput[0]);exit;
-  
+$dom->loadHTML();
+
+$xpath = new DOMXPath($dom);
+
+preg_match('@<(div|em) class=\"maincounter-number\" (style=\"(.+?)\")>(.+?)</\1>@is', $getData, $recovered);
+preg_match_all('@<(div|em) class=\"maincounter-number\">(.+?)</\1>@is', $getData, $matches);
+
+
+
+$output['total_cases'] = $matches[0][1];
+$output['total_deaths'] = $matches[0][0];
+$output['total_recovered'] = $recovered[0];
+$output = json_encode($output);
+$filenametotal = "total.txt";
+if(file_exists($filenametotal) && (filemtime($filenametotal) + (60 * 60)) < time())
+{
+    
+} else {
+    $myfileTotal = fopen($filenametotal, "w+") or die("Unable to open file!");
+    fwrite($myfileTotal, $output);
+    fclose($myfileTotal);
+}
 ?>
